b24e114cf477ac4742286120e811ac8de672f5fe
yangcock <yangcock@yangcocks-iMac.local>
2017-11-15 11:30:36 +0800

    aa

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -31,6 +31,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+		//222
     }
 
 
