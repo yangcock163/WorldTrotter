e5b054bb3ceacd612c2b2b596acc6ad443b624fe
yangcock <yangcock@yangcocks-iMac.local>
2017-11-15 10:54:37 +0800

    commit-1-yckTest

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+		super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+		
+		/*
+        // 添加视图模1
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height:150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
+        // 添加视图2
+        let secondFrame = CGRect(x:20, y:20, width: 50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.green
+        //view.addSubview(secondView)
+		firstView.addSubview(secondView)
+		*/
+	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
